add65c649cdc2a4e67f73bc21ccaac130253daf8
Lambda <anwar.essafar@gmail.com>
2023-03-15 15:58:38 +0100

    the class MyFrame that extends JFrame

---
+++ src/MyFrame.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class MyFrame extends JFrame {
+
+    public MyFrame(){
+        setVisible(true);
+        setResizable(false);
+        setBounds(10 , 40 , 1300 , 700);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);    }
+
+}
