549dfa781d270edd7374c4de857ab8212b42df88
Lambda <anwar.essafar@gmail.com>
2023-03-15 23:13:06 +0100

    I updated MyFrame class, changed the background color

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -1,11 +1,13 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class MyFrame extends JFrame {
 
     public MyFrame(){
+        getContentPane().setBackground(new Color(236, 242, 255));
         setVisible(true);
         setResizable(false);
         setBounds(10 , 40 , 1300 , 700);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);    }
-
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
 }
