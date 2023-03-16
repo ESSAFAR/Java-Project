4b2c0c28dff0e62dcf1e5410197b95e27fb35dd6
Lambda <anwar.essafar@gmail.com>
2023-03-16 23:17:48 +0100

    Login frame

--- src/com/GUI/Login.java
+++ src/com/GUI/Login.java
@@ -12,13 +12,13 @@ public class Login extends MyFrame {
         setTitle("Login");
 
         //Log in button
-        MyButtons loginBtn = new MyButtons("Log in" , Color.BLUE , Color.WHITE , 150 , 600 , 250 , 50) ;
+        MyButtons loginBtn = new MyButtons("Log in" , Color.BLUE , Color.WHITE , 170 , 650 , 250 , 50) ;
         add(loginBtn);
 
 
         //Side image
         JPanel imageContainer = new JPanel();
-        imageContainer.setBounds(700 , 40 , 750 , 650);
+        imageContainer.setBounds(700 , 0 , 790 , 770);
         JLabel sideImage = new JLabel();
         sideImage.setIcon(new ImageIcon("images/sideImage.jpg"));
         imageContainer.add(sideImage);
@@ -29,10 +29,10 @@ public class Login extends MyFrame {
 
         //Logo
         JPanel logoContainer = new JPanel();
-        logoContainer.setBounds(120 , 90 , 350 , 70);
+        logoContainer.setBounds(90 , 50 , 400 , 158);
         JLabel logo = new JLabel();
-        logo.setIcon(new ImageIcon("images/SchoolEase3.png"));
-        logoContainer.setBackground(Color.red);
+        logo.setIcon(new ImageIcon("images/SchoolEase4resized.png"));
+        logoContainer.setBackground(new Color(0,0,0,0));
         logoContainer.add(logo);
         add(logoContainer);
 
@@ -41,31 +41,49 @@ public class Login extends MyFrame {
         JPanel connectez = new JPanel();
         connectez.setBounds(120 , 300 , 350 , 70);
         JLabel connectezText = new JLabel();
-        connectezText.setText("Connectz vous");
-        connectez.setBackground(Color.yellow);
+        connectezText.setText("Connectez vous");
+        connectezText.setFont(new Font(Font.SANS_SERIF, Font.BOLD,  38));
+        connectezText.setForeground(Color.red);
+        connectez.setBackground(new Color(0,0,0,0));
         connectez.add(connectezText);
+        connectezText.setFont(new Font(Font.SANS_SERIF, Font.BOLD,  30));
+
         add(connectez);
 
 
         //user identifiant
         JPanel identifiant = new JPanel();
-        identifiant.setBackground(Color.cyan);
         JLabel identifiantText = new JLabel();
         identifiantText.setText("Entrer votre ID");
-        identifiant.setBounds(120 , 400 , 180 , 30);
+        identifiantText.setFont(new Font(Font.SANS_SERIF, Font.BOLD,  17));
+        identifiant.setBounds(58 , 400 , 250 , 30);
+        identifiant.setBackground(new Color(0,0,0,0));
         identifiant.add(identifiantText);
         add(identifiant);
+
+
+        //identifiant text field
+        JTextField identifiantField = new JTextField();
+        identifiantField.setBounds(120 , 435 , 180 , 30);
+        add(identifiantField);
 
 
 
         //password
         JPanel passwordPanel = new JPanel();
-        passwordPanel.setBackground(Color.cyan);
+        passwordPanel.setBackground(new Color(0,0,0,0));
         JLabel passwordText = new JLabel();
         passwordText.setText("Entrer votre mot de passe");
-        passwordPanel.setBounds(120 , 530 , 180 , 30);
+        passwordText.setFont(new Font(Font.SANS_SERIF, Font.BOLD,  17));
+        passwordPanel.setBounds(103 , 500 , 250 , 30);
         passwordPanel.add(passwordText);
         add(passwordPanel);
+
+
+        //password field
+        JPasswordField passwordField = new JPasswordField();
+        passwordField.setBounds(120 , 535 , 180 , 30);
+        add(passwordField);
 
 
 
