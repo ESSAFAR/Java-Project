37ce718319695076ea32c5955f6e0717816a7b42
Lambda <anwar.essafar@gmail.com>
2023-03-23 10:29:10 +0000

    design des 3 primers frames

--- src/com/GUI/AdminFrames/AdminFrame1.java
+++ src/com/GUI/AdminFrames/AdminFrame1.java
@@ -18,17 +18,17 @@ public class AdminFrame1 extends MyFrame {
     public AdminFrame1(){
 
         //Logo
-        JPanel logoContainer = new JPanel();
-        logoContainer.setBounds(90 , 50 , 400 , 158);
-        JLabel logo = new JLabel();
-        logo.setIcon(new ImageIcon("images/SchoolEase4resized.png"));
-        logoContainer.setBackground(new Color(0,0,0,0));
-        logoContainer.add(logo);
-        add(logoContainer);
+        //JPanel logoContainer = new JPanel();
+        //logoContainer.setBounds(90 , 50 , 400 , 158);
+        //JLabel logo = new JLabel();
+        //logo.setIcon(new ImageIcon("images/SchoolEase4resized.png"));
+        //logoContainer.setBackground(new Color(0,0,0,0));
+        //logoContainer.add(logo);
+        //add(logoContainer);
 
 
         //Gestion eleves
-        MyButtons btn_eleves = new MyButtons("Gestion eleves" , Color.blue , Color.white , 190 , 250 , 250 , 50);
+        MyButtons btn_eleves = new MyButtons("Gestion eleves" , Color.blue , Color.white , 50 , 380 , 250 , 50);
         add(btn_eleves);
         btn_eleves.addActionListener(e -> {
             dispose();
@@ -41,26 +41,26 @@ public class AdminFrame1 extends MyFrame {
 
 
         //Gestion prof
-        MyButtons btn_professeurs = new MyButtons("Gestion professeurs" , Color.blue , Color.white ,190 , 320 , 250 , 50);
+        MyButtons btn_professeurs = new MyButtons("Gestion professeurs" , Color.blue , Color.white ,570 , 380 , 250 , 50);
         add(btn_professeurs);
 
 
 
         //Gestion notes
-        MyButtons btn_notes = new MyButtons("Gestion Notes" , Color.blue , Color.white , 190 , 420 , 250 , 50);
+        MyButtons btn_notes = new MyButtons("Gestion Notes" , Color.blue , Color.white , 290 , 530 , 250 , 50);
         add(btn_notes);
 
 
         //Communiquer un avis
-        MyButtons btn_avis= new MyButtons("Gestion avis" , Color.blue , Color.white , 190 , 490 , 250 , 50);
+        MyButtons btn_avis= new MyButtons("Gestion avis" , Color.blue , Color.white , 700 , 530 , 250 , 50);
         add(btn_avis);
 
 
-        //Side image
+        //centre image
         JPanel imageContainer = new JPanel();
         imageContainer.setBounds(600 , 0 , 920 , 770);
         JLabel sideImage = new JLabel();
-        sideImage.setIcon(new ImageIcon("images/sideImage.jpg"));
+        sideImage.setIcon(new ImageIcon("images/2.jpeg"));
         imageContainer.add(sideImage);
         add(imageContainer);
 
--- src/com/Style/MyFrame.java
+++ src/com/Style/MyFrame.java
@@ -5,8 +5,8 @@ import java.awt.*;
 
 public class MyFrame extends JFrame {
 
-    public static int frameWidth = 1200;
-    public static int frameHeight = 700;
+    public static int frameWidth = 1050;
+    public static int frameHeight = 650;
 
 
     /*
@@ -14,11 +14,12 @@ public class MyFrame extends JFrame {
     */
 
     public MyFrame(){
-        getContentPane().setBackground(new Color(236, 242, 255));
+        getContentPane().setBackground(new Color(255, 255, 255));
+
         setResizable(false);
         setBounds(0 , 0 , frameWidth , frameHeight);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setLayout(null);
+        //setLayout(null);
         ImageIcon logo = new ImageIcon("images\\SchoolEase2.png");
         setIconImage(logo.getImage());
     }
